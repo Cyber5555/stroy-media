6cdac469cf2193fad86653b58cd9dbaacb271ba6
Cyber5555 <lagagaga05@gmail.com>
2023-03-21 12:38:33 +0400

    new-changes-21.03

--- metro.config.js
+++ metro.config.js
@@ -5,7 +5,7 @@ const { getDefaultConfig } = require('expo/metro-config');
 module.exports = getDefaultConfig(__dirname);
 */
 
-const { getDefaultConfig } = require("expo/metro-config");
+const { getDefaultConfig } = require("@expo/metro-config");
 
 module.exports = (async () => {
   const {
@@ -14,6 +14,7 @@ module.exports = (async () => {
   return {
     transformer: {
       babelTransformerPath: require.resolve("react-native-svg-transformer"),
+      assetPlugins: ["expo-asset/tools/hashAssetFiles"],
     },
     resolver: {
       assetExts: assetExts.filter((ext) => ext !== "svg"),
